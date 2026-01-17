b6d4d4d16c7f0225e3ac6045ed656a23debe6a3f
Hazem <hazem.abdeen39@gmail.com>
2026-01-17 11:05:12 +0300

    first view

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Exam
 
-# Create your views here.
+@login_required
+def exam_list(request):
+    exams = Exam.objects.select_related("course").all().order_by("-id")
+    return render(request, "exams/exam_list.html", {"exams": exams})
